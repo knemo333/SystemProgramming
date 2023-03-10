f533cb270b7e49d5b3c72c760c1c5725b89138ff
kkangddol_KGIT <knemo333@gmail.com>
2023-03-10 16:08:13 +0900

    finddata 제거하고 filesystem으로 변경
    finddata는 C 스펙
    filesystem은 C++17 스펙이므로
    filesystem 사용하는 걸 익히는 겸 변경했음.

--- WatchJSON/WatchJSON/main.cpp
+++ WatchJSON/WatchJSON/main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
+#include <filesystem>
 #include <windows.h>
 #include <vector>
-#include <io.h>
+#include <regex>
+
+namespace fs = std::filesystem;
 
 constexpr int BUFFERSIZE = 256;
 
 std::wstring ReadDirectory();
-std::vector<std::string> get_files_inDirectory(const std::string& _path, const std::string& _filter);
 
 int main()
 {
 	std::wstring dir;
 	dir = ReadDirectory();
 
-	std::wcout << dir << std::endl;
-
-	std::string dir_a;
-	dir_a.assign(dir.begin(), dir.end());
-
-	std::vector<std::string> files = get_files_inDirectory(dir_a, "*.json");
-
-	for (auto& file : files)
+	fs::path jsonPath(dir);
+	fs::directory_iterator iter(dir);
+	while (iter != fs::end(iter))
 	{
-		std::cout << file << std::endl;
+		const fs::directory_entry& entry = *iter;
+		std::cout << entry.path() << std::endl;
+		++iter;
 	}
 
+	Sleep(2000);
 	return 0;
 }
 
@@ -37,33 +37,4 @@ std::wstring ReadDirectory()
 	GetPrivateProfileString(L"JSON", L"src", L"babo", directory, BUFFERSIZE, L".\\setting.ini");
 
 	return directory;
-}
-
-// find all matched file in specified directory
-// [INPUT]
-//   >> const std::string& _path        Search path        ex) c:/directory/
-//   >> const std::string& _filter        Search filter    ex) *.exe or *.*
-// [RETURN]
-//   >> std::vector<std::string>        All matched file name & extension
-std::vector<std::string> get_files_inDirectory(const std::string& _path, const std::string& _filter)
-{
-	std::string searching = _path + _filter;
-
-	std::vector<std::string> return_;
-
-	_finddata_t fd;
-	long handle = _findfirst(searching.c_str(), &fd);  //현재 폴더 내 모든 파일을 찾는다.
-
-	if (handle == -1)    return return_;
-
-	int result = 0;
-	do
-	{
-		return_.push_back(fd.name);
-		result = _findnext(handle, &fd);
-	} while (result != -1);
-
-	_findclose(handle);
-
-	return return_;
 }
